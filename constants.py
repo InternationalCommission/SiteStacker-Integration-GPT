5e61209ad3960968375e5714d4c97d8b39a3d28c
InternationalCommission <133237944+InternationalCommission@users.noreply.github.com>
2023-05-11 13:28:05 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,16 @@
+# Constants for SiteStacker API
+SS_API_BASE_URL = 'https://ic-world.wmtekdev.com/api/'
+SS_PERSON_TYPE = 'person'
+
+# Constants for Constant Contact API
+CC_API_BASE_URL = 'https://api.cc.email/v3/'
+CC_CONTACT_TYPE = 'contacts'
+
+# Scopes required for Constant Contact API
+CC_SCOPES = [
+    'contacts_read',
+    'contacts_write',
+    'campaigns_read',
+    'campaigns_write',
+    'account_read'
+]
